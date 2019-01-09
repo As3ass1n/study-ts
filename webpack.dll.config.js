a0a1d0537cad70a33a84a665309e37706aab9c06
assassin <811273998@qq.com>
2019-01-09 17:22:07 +0800

    dll打包添加部分公共库

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,7 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
 
-const vendors = ["react", "react-dom"];
+const vendors = [
+  "react",
+  "react-dom",
+  "redux",
+  "react-redux",
+  "react-router-dom"
+];
 
 const config = {
   entry: {
@@ -10,12 +16,12 @@ const config = {
   output: {
     path: path.resolve(__dirname, "dist/dll"),
     filename: "[name].dll.js",
-    library: "[name]_[hash:8]"
+    library: "vendors_lib"
   },
   plugins: [
     new webpack.DllPlugin({
       path: path.resolve(__dirname, "dist/dll", "[name].manifest.json"),
-      name: "[name]_[hash:8]"
+      name: "vendors_lib"
     })
   ]
 };
