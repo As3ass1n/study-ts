bd57cbfd3a4aff52486375b9b457f8095ae0fc65
assassin <811273998@qq.com>
2019-01-11 19:10:00 +0800

    抽离App.tsx 添加redux

--- src/todo/index.tsx
+++ src/todo/index.tsx
@@ -1,164 +1,18 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
 import "./index.less";
+import reducers from "./reducers";
 
-interface IState {
-  value: string;
-  todoList: Array<{ value: string; completed: boolean }>;
-  completeList: any[];
-  filter: string;
+const store = createStore(reducers);
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 }
 
-const Filters = {
-  SHOW_ACTIVE: "SHOW_ACTIVE",
-  SHOW_ALL: "SHOW_ALL",
-  SHOW_COMPLETED: "SHOW_COMPLETED"
-};
-
-class App extends Component<{}, IState> {
-  public state: IState = {
-    completeList: [],
-    filter: Filters.SHOW_ALL,
-    todoList: [{ value: "123", completed: false }],
-    value: ""
-  };
-
-  public render() {
-    const { todoList, value, completeList, filter } = this.state;
-    console.log(completeList);
-    const visibleTodos = {
-      SHOW_ACTIVE: todoList.filter(({ completed }) => !completed),
-      SHOW_ALL: todoList,
-      SHOW_COMPLETED: todoList.filter(({ completed }) => completed)
-    };
-    return (
-      <div>
-        <div>
-          <form onSubmit={this.handleTodoAdd}>
-            <input
-              onChange={this.handleInput}
-              value={value}
-              className="new-todo"
-            />
-          </form>
-        </div>
-        <section className="main">
-          <ul className="todo-list">
-            {visibleTodos[filter].map((todo, index) => {
-              return (
-                <li
-                  key={todo.value}
-                  className={todo.completed ? "completed" : ""}
-                >
-                  <div className="view">
-                    <input
-                      className="toggle"
-                      type="checkbox"
-                      checked={todo.completed}
-                      onChange={this.handleComplete(index)}
-                    />
-                    <label className={todo.completed ? "text" : ""}>
-                      {todo.value}
-                    </label>
-                    <button
-                      className="destroy"
-                      onClick={this.deleteTodo(index)}
-                    />
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </section>
-        {visibleTodos.SHOW_ACTIVE.length ||
-        visibleTodos.SHOW_COMPLETED.length ? (
-          <footer className="footer">
-            <ul className="filters">
-              <li>
-                <a
-                  href=""
-                  onClick={this.filterTodo(Filters.SHOW_ALL)}
-                  className={filter === Filters.SHOW_ALL ? "selected" : ""}
-                >
-                  All
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  onClick={this.filterTodo(Filters.SHOW_ACTIVE)}
-                  className={filter === Filters.SHOW_ACTIVE ? "selected" : ""}
-                >
-                  Active
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  onClick={this.filterTodo(Filters.SHOW_COMPLETED)}
-                  className={
-                    filter === Filters.SHOW_COMPLETED ? "selected" : ""
-                  }
-                >
-                  Completed
-                </a>
-              </li>
-            </ul>
-          </footer>
-        ) : null}
-      </div>
-    );
-  }
-
-  private filterTodo = (filterType: string) => {
-    return (e: any) => {
-      e.preventDefault();
-      this.setState({
-        filter: filterType
-      });
-    };
-  };
-
-  private handleComplete = (index: number) => {
-    return (e: any) => {
-      const { todoList } = this.state;
-      console.log(e);
-      this.setState({
-        todoList: todoList.map((todo, i) => {
-          if (i === index) {
-            return {
-              completed: e.target.checked,
-              value: todo.value
-            };
-          }
-          return todo;
-        })
-      });
-    };
-  };
-
-  private handleTodoAdd = (e: any) => {
-    e.preventDefault();
-    const { todoList, value } = this.state;
-    this.setState({
-      todoList: todoList.concat({ value, completed: false }),
-      value: ""
-    });
-  };
-
-  private deleteTodo = (index: number) => {
-    return () => {
-      const { todoList } = this.state;
-      this.setState({
-        todoList: todoList.filter((item, i) => index !== i)
-      });
-    };
-  };
-
-  private handleInput = (e: any) => {
-    this.setState({
-      value: e.target.value
-    });
-  };
-}
-
-export default App;
+export default Root;
