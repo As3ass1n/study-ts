fb97b059c6bb695fb87b733416eaaa34f3f9532a
assassin <811273998@qq.com>
2019-01-23 12:04:53 +0800

    父组件传入props提示问题

--- src/todo/contianers/Header.tsx
+++ src/todo/contianers/Header.tsx
@@ -1,13 +1,14 @@
-import React, { Component, createRef } from "react";
+import React, { Component, ComponentClass, createRef } from "react";
+// import { connect } from "utils/connect";
+import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { addTodo } from "todo/actions";
-import { connect } from "utils/connect";
 
-interface IProps {
-  dispatch: Dispatch;
+interface IDispatchProps {
+  addToDo: (text: string) => void;
 }
 
-class Header extends Component<IProps> {
+class Header extends Component<IDispatchProps> {
   protected inputRef = createRef<HTMLInputElement>();
 
   public render() {
@@ -22,10 +23,21 @@ class Header extends Component<IProps> {
 
   private handleTodoAdd = (e: any) => {
     e.preventDefault();
-    const { dispatch } = this.props;
-    dispatch(addTodo(this.inputRef.current.value));
+    const { addToDo } = this.props;
+    addToDo(this.inputRef.current.value);
     this.inputRef.current.value = "";
   };
 }
 
-export default connect()(Header);
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = (dispatch: Dispatch): IDispatchProps => {
+  return {
+    addToDo: (text: string) => dispatch(addTodo(text))
+  };
+};
+
+export default connect<{}, IDispatchProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header) ;
