a6b40328ee397b32c79356cbf4b30f3dff816ceb
assassin <811273998@qq.com>
2019-01-25 14:57:37 +0800

    todolist add deleteTodo handleComplete function

--- src/todo/contianers/TodoList.tsx
+++ src/todo/contianers/TodoList.tsx
@@ -1,28 +1,38 @@
 import React, { ChangeEvent, Component, MouseEvent } from "react";
-import { connect } from "utils/connect";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { completeTodo, deleteTodo } from "todo/actions";
+import { SHOW_ACTIVE, SHOW_ALL } from "todo/constants/TodoFilters";
+import { IAppState } from "todo/reducers";
 import TodoItem from "../components/TodoItem";
 
-interface IProps {
-  todos: Array<{ value: string; completed: boolean }>;
+interface Itodo {
+  value: string;
+  completed: boolean;
+  id: number;
 }
 
-const mapStateToProps = (state: any) => {
-  return { todos: [...state] };
-};
+interface IStateProps {
+  todos: Itodo[];
+}
 
-@connect(mapStateToProps)
-class TodoList extends Component<IProps> {
+interface IDispatchProps {
+  deleteToDo: (id: number) => void;
+  handleComplete: (id: number) => void;
+}
+
+class TodoList extends Component<IStateProps & IDispatchProps> {
   public render() {
     const { todos } = this.props;
     return (
       <ul className="todo-list">
-        {todos.map((todo, index) => {
+        {todos.map(todo => {
           return (
             <TodoItem
               todo={todo}
-              key={todo.value}
-              handleComplete={this.handleComplete(index)}
-              deleteTodo={this.deleteTodo(index)}
+              key={todo.id}
+              handleComplete={this.handleComplete(todo.id)}
+              deleteTodo={this.deleteTodo(todo.id)}
             />
           );
         })}
@@ -30,17 +40,43 @@ class TodoList extends Component<IProps> {
     );
   }
 
-  private handleComplete = (index: number) => {
-    return (e: ChangeEvent) => {
-      console.log(e);
+  private handleComplete = (id: number) => {
+    return () => {
+      const { handleComplete } = this.props;
+      handleComplete(id);
     };
   };
 
   private deleteTodo = (index: number) => {
     return (e: MouseEvent) => {
-      console.log(e);
+      const { deleteToDo } = this.props;
+      deleteToDo(index);
     };
   };
 }
 
-export default TodoList;
+const mapStateToProps = (state: IAppState): IStateProps => {
+  let todos: Itodo[];
+  const { visibilityFilters } = state;
+  if (visibilityFilters === SHOW_ALL) {
+    todos = [...state.todos];
+  } else if (visibilityFilters === SHOW_ACTIVE) {
+    todos = state.todos.filter(todo => !todo.completed);
+  } else {
+    todos = state.todos.filter(todo => todo.completed);
+  }
+  console.log(todos);
+  return { todos };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): IDispatchProps => {
+  return {
+    deleteToDo: id => dispatch(deleteTodo(id)),
+    handleComplete: id => dispatch(completeTodo(id))
+  };
+};
+
+export default connect<IStateProps, IDispatchProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
