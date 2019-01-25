60d3479ffd6bd9cc88690d278c2cb56cce3bd99e
assassin <811273998@qq.com>
2019-01-25 14:56:09 +0800

    todo status change，filter complete todo

--- src/todo/actions/index.ts
+++ src/todo/actions/index.ts
@@ -1,14 +1,38 @@
+import {
+  ADD_TODO,
+  COMPLETE_TODO,
+  DELETE_TODO,
+  SET_VISIBILITY_FILTER
+} from "todo/constants/ActionTypes";
+
+let count: number = 0;
+
 export const addTodo = (text: string) => {
   console.log(text);
   return {
-    type: 'ADD_TODO',
-    value: text,
+    id: count++,
+    type: ADD_TODO,
+    value: text
   };
 };
 
-
-const deleteTodo = () => {
+export const completeTodo = (id: number) => {
   return {
-    type: 'DELETE_TODO'
-  }
-}
+    id,
+    type: COMPLETE_TODO
+  };
+};
+
+export const deleteTodo = (id: number) => {
+  return {
+    id,
+    type: DELETE_TODO
+  };
+};
+
+export const setVisibilityFilter = (filter: string) => {
+  return {
+    filter,
+    type: SET_VISIBILITY_FILTER
+  };
+};
